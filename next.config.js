033ddb7302c2d3bff84e82116ce3cb8b0e424a46
nakanoamane <amane.nakano@kufusumai.co.jp>
2024-01-09 10:54:53 +0900

    image path

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
-const isProd = process.env.NODE_ENV === 'production'
-const prefixPath = !isProd ? '/farm' : ''
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	assetPrefix: prefixPath,
-	basePath: prefixPath,
 	output: 'export',
-	images: { unoptimized: true }
+	images: { unoptimized: true },
 }
 
 module.exports = nextConfig
