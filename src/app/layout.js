c8c25e2ca3812021d7357e6b3ed23d8cfc62c8f3
nakanoamane <amane.nakano@kufusumai.co.jp>
2024-02-06 00:09:31 +0900

    metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,11 @@
 import 'the-new-css-reset/css/reset.css';
 import '../styles/base.scss';
+import { basePath } from '../../next.config'
 
 export const metadata = {
   title: 'Farm',
   description: 'The game of farming',
+  metadataBase: new URL(basePath ? basePath : 'http://localhost:3000'),
 }
 
 export default function RootLayout({ children }) {
